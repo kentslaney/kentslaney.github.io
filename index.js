5e8917c8e6cc8f2a9661c04fba7d8ef4ae0c747b
Kent Slaney <kent@slaney.org>
2025-05-23 16:29:44 -0700

    icon scaling

--- index.js
+++ index.js
@@ -46,14 +46,23 @@ window.addEventListener("load", () => {
   const selectable = document.querySelector("#ks-select")
   const container = document.querySelector("#scroll-index")
   const parent = document.querySelector("#ks-case")
+  const iconSwitch = window.getComputedStyle(document.documentElement)
+    .getPropertyValue("--ks-switch")
   const scroll = () => {
     const { top } = container.getBoundingClientRect();
     const pos = (window.innerHeight - top) / window.innerHeight
+    parent.style.setProperty("--splash-scroll", pos)
     if (pos > 0.05) selectable.classList.add("hidden")
     else selectable.classList.remove("hidden")
-    if (pos > 0.65) parent.classList.add("icon-svg")
+    if (pos > iconSwitch) parent.classList.add("icon-svg")
     else parent.classList.remove("icon-svg")
   }
   window.addEventListener("scroll", scroll, { passive: true })
   scroll()
+  const resize = () => {
+    document.documentElement.style.setProperty(
+      "--page-ratio", window.innerWidth / window.innerHeight)
+  }
+  window.addEventListener("resize", resize, { passive: true })
+  resize()
 })
